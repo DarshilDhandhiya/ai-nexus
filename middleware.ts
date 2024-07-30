daff57539d54fc5bfb6dd53d54d765661161d855
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-07-30 14:47:51 +0530

    Auth Complete

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: ["/"]
+});
+
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
