124cb32e4ae40106683d784be2ba57ecec749ca1
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-07-30 19:34:10 +0530

    Sidebar Created

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import {UserButton} from "@clerk/nextjs";
+// import {UserButton} from "@clerk/nextjs";
 
 const DashboardPage = () => {
     return (
       <div>
         <p>Dashboard Page (Protected)</p>
-        <UserButton afterSignOutUrl="/" />
+        {/* <UserButton afterSignOutUrl="/" /> */}
       </div>
       );
 }
