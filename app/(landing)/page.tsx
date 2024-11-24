2eb6ebbfa0329d7f347727e9260237176e067d07
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-11-24 11:56:45 +0530

    Landing Page is Done

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,24 +1,41 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+
+// const LandingPage = () => {
+//     return ( 
+//         <div>
+//             Landing Page (Unprotected)
+//             <div>
+//                 <Link href="/sign-in">
+//                     <Button>
+//                         Login
+//                     </Button>
+//                 </Link>
+//                 <Link href="/sign-up">
+//                     <Button>
+//                         Register
+//                     </Button>
+//                 </Link>
+//             </div>
+//         </div>
+//      );
+// }
+ 
+// export default LandingPage;
+import { LandingContent } from "@/components/landing-content";
+import { LandingFooter } from "@/components/landing-footer";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
-    return ( 
-        <div>
-            Landing Page (Unprotected)
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
-            </div>
-        </div>
-     );
-}
- 
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+      <LandingFooter />
+    </div>
+  );
+};
+
 export default LandingPage;
